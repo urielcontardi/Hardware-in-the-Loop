e8c2150a471be78c03e555266ae0896505043acf
urielcontardi <urielcontardi@hotmail.com>
2024-08-04 22:08:24 -0300

    Finalization of StateSpaceSolver

---
+++ syn/HIL.ip_user_files/ip/StateSolverAdder/StateSolverAdder_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+-- Date        : Tue Jun 25 13:14:13 2024
+-- Host        : BRJGSD317426 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/contardii/Desktop/Hardware-in-the-Loop/syn/HIL.gen/sources_1/ip/StateSolverAdder/StateSolverAdder_stub.vhdl
+-- Design      : StateSolverAdder
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s100fgga676-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity StateSolverAdder is
+  Port ( 
+    A : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    B : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    CLK : in STD_LOGIC;
+    S : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end StateSolverAdder;
+
+architecture stub of StateSolverAdder is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "A[31:0],B[31:0],CLK,S[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "c_addsub_v12_0_14,Vivado 2021.2";
+begin
+end;
